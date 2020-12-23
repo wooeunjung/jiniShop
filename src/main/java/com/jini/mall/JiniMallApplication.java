f8e4afe85d88fea8ca0709333bf2357d2a3aa903
KangJiyeon <kangjy@mih.co.kr>
2020-12-23 12:34:47 +0900

    project 생성

---
+++ src/main/java/com/jini/mall/JiniMallApplication.java
@@ -0,0 +1,13 @@
+package com.jini.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiniMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JiniMallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jini/mall/JiniMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jini.mall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiniMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
